ffeec97071b626e49826f859e28c51f79a3f58ee
fhenningsen <felix.henningsen@tum.de>
2021-01-14 06:36:43 -0600

    added test data set

---
+++ generation/sim_executable.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+#env:
+# eval $(/cvmfs/icecube.opensciencegrid.org/py2-v3.1.1/setup.sh)
+# metaproject='/home/fschmuckermaier/IceCube/meta-projects/combo/stable/build'
+eval $(/cvmfs/icecube.opensciencegrid.org/py2-v3.1.1/setup.sh)
+metaproject='/home/fhenningsen/osc/py2-ext/build'
+
+### 1. DEFINE INPUT PARAMETER ###
+
+# number of runs
+N=$1
+
+# output file
+file_name=$2
+
+# Here, we define the 5 key parameters or rather define the lists of parameters we want to iterate through
+water_shift_abs=$3
+water_shift_sca=$4
+dom_eff_corr=$5
+#p0_ang_acc=$6
+#p1_ang_acc=$7
+phot=$6
+
+# geometry file
+gcd=$7
+
+### 2. APPLYING THE PARAMETERS BY EDITING FILES ###
+# Then we apply the parameters by editing the necessary files
+
+# modification scripts
+mod_scripts="/home/fhenningsen/pone/pone_geo-cal/modification_scripts"
+
+# default water tables
+water_default="/home/fhenningsen/pone/pone_geo-cal/watermodel/default"
+
+# copy all the tables files so that a new submission doesnt change things
+# and create a new tmp directory for the current submission
+tmp_dir="tmp-a$3-b$4-de$5"
+# make sure that truth is not deleted after normal jobs finished running and parameters were the same
+if [[ $file_name == *"TRUTH"* ]]; then
+    water_tables="/home/fhenningsen/pone/pone_geo-cal/watermodel/tmp/truth_${tmp_dir}"
+else
+    water_tables="/home/fhenningsen/pone/pone_geo-cal/watermodel/tmp/${tmp_dir}"
+fi
+# and copy all default files
+mkdir -p $water_tables
+cp ${water_default}/* ${water_tables}/.
+
+# Multiplying the offset to the watermodel parameters:
+$metaproject/env-shell.sh python ${mod_scripts}/change_water.py --a_corr=$water_shift_abs --b_corr=$water_shift_sca --outfile=${water_tables}/icemodel.dat
+
+# Replace the overall dom efficiency factor in cfg.txt (Note: check if values greater than 1 work!):
+sed "3s/.*/$dom_eff_corr/" ${water_default}/cfg.txt > ${water_tables}/cfg.txt
+
+# taking p0 & p1 as input and produce an as.dat file in the water directory
+# format: dima_from_unified.py <p0> <p1> <out_file>
+# python ${mod_scripts}/dima_from_unified.py "$p0_ang_acc" "$p1_ang_acc" "$water_tables/as.dat"
+
+### 3. RUN THE SIMULATION ###
+#Then we run the simulation for all 7 relevant POCAMs with the new files
+
+### Configure:  ###
+
+### Running through all 7 POCAMs: ###
+string=87
+dom=84
+$metaproject/env-shell.sh python /home/fhenningsen/pone/pone_geo-cal/generation/simple_POCAM_simulation.py --gcd-file=$gcd  --output-i3-file="${file_name}" --number-of-photons=$phot --number-of-runs=$N --string=$string --dom=$dom --ice-tables=$water_tables
+
+string=88
+dom=72
+$metaproject/env-shell.sh python /home/fhenningsen/pone/pone_geo-cal/generation/simple_POCAM_simulation.py --gcd-file=$gcd  --output-i3-file="${file_name}" --number-of-photons=$phot --number-of-runs=$N --string=$string --dom=$dom --ice-tables=$water_tables
+
+string=89
+dom=38
+$metaproject/env-shell.sh python /home/fhenningsen/pone/pone_geo-cal/generation/simple_POCAM_simulation.py --gcd-file=$gcd  --output-i3-file="${file_name}" --number-of-photons=$phot --number-of-runs=$N --string=$string --dom=$dom --ice-tables=$water_tables
+
+string=90
+dom=100
+$metaproject/env-shell.sh python /home/fhenningsen/pone/pone_geo-cal/generation/simple_POCAM_simulation.py --gcd-file=$gcd  --output-i3-file="${file_name}" --number-of-photons=$phot --number-of-runs=$N --string=$string --dom=$dom --ice-tables=$water_tables
+
+string=91
+dom=50
+$metaproject/env-shell.sh python /home/fhenningsen/pone/pone_geo-cal/generation/simple_POCAM_simulation.py --gcd-file=$gcd  --output-i3-file="${file_name}" --number-of-photons=$phot --number-of-runs=$N --string=$string --dom=$dom --ice-tables=$water_tables
+
+string=92
+dom=28
+$metaproject/env-shell.sh python /home/fhenningsen/pone/pone_geo-cal/generation/simple_POCAM_simulation.py --gcd-file=$gcd  --output-i3-file="${file_name}" --number-of-photons=$phot --number-of-runs=$N --string=$string --dom=$dom --ice-tables=$water_tables
+
+string=93
+dom=64
+$metaproject/env-shell.sh python /home/fhenningsen/pone/pone_geo-cal/generation/simple_POCAM_simulation.py --gcd-file=$gcd  --output-i3-file="${file_name}" --number-of-photons=$phot --number-of-runs=$N --string=$string --dom=$dom --ice-tables=$water_tables
+
+# remove the current water dir
+#rm -rf $water_tables
---
+++ generation/submit-pocam.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+import numpy as np
+import datetime
+from os.path import join, exists
+
+# who
+datauser = '/data/user/fhenningsen'
+
+# executable
+executable = '/home/fhenningsen/pone/pone_geo-cal/generation/sim_executable.sh'
+mem        = 10 #GB
+
+# tag for simulation
+sim_tag = 'water_test'
+
+# create time-sensitive folder for output
+t_str = datetime.datetime.now().isoformat('_')[:-7].replace(':', '-')
+out_folder = join(datauser, 'pone_data' , 'sim_' + sim_tag + '_' + t_str)
+if not exists(out_folder):
+    print('Creating output directory in {}'.format(out_folder))
+    os.makedirs(out_folder)
+
+# create time-sensitive folder for log files
+out_log = join('/scratch/fhenningsen', 'logs', t_str)
+if not exists(out_log):
+    print('Creating log directory in {}'.format(out_log))
+    os.makedirs(out_log)
+
+# create time-sensitive folder for log files
+out_sub = join('/scratch/fhenningsen', 'subs', t_str)
+if not exists(out_sub):
+    print('Creating log directory in {}'.format(out_sub))
+    os.makedirs(out_sub)
+
+# fixed inputs
+N        = 100
+N_photon = 1e9
+gcd      = "/home/fhenningsen/gcd/physics_volume_GCD.i3.bz2"
+
+# submit 
+submit_data  = True
+
+# running config parameters
+n_par = 3
+ABS   = np.linspace(0.5, 1.5,    n_par)
+SCA   = np.linspace(0.5, 1.5,    n_par)
+DOME  = np.linspace(0.5, 1.5,    n_par)
+
+# fill parameter grid
+grid_filled = np.zeros((len(ABS) * len(SCA) * len(DOME), 3))
+
+cntr = 0
+for abso in ABS:
+    for sca in SCA:
+        for dome in DOME:
+            grid_filled[cntr] = [abso, sca, dome]
+            cntr += 1
+
+grid_filled = np.round(grid_filled, 5)
+np.save(join(out_folder, 'GRID.py'), grid_filled)
+
+print('Number of jobs: %i' %(len(ABS) * len(SCA) * len(DOME)))
+
+# simulation sets
+for abso in ABS:
+
+    for sca in SCA:
+
+        for dome in DOME:
+
+            out_file    = 'GCD_%s_ABS-%.3f_SCA-%.3f_DOME-%.3f_NPH-%.3e_N-%i' %(gcd.split('/')[-1], abso, sca, dome, N_photon, N)
+            args        = "%i %s %.3f %.3f %.3f %i %s" %(N, join(out_folder, out_file), abso, sca, dome, N_photon, gcd)
+            env         = "HDF5_USE_FILE_LOCKING='FALSE'"
+            log_str     = 'job_deepcore_%s' %(out_file)
+            submit_info = 'executable  = {script} \n\
+                universe    = vanilla \n\
+                initialdir = /home/fhenningsen \n\
+                request_gpus = 1 \n\
+                request_memory = {mem}GB \n\
+                log         = {outl}/{logs}.log \n\
+                output      = {out}/{logs}.out \n\
+                error       = {out}/{logs}.err \n\
+                arguments   = "{args}" \n\
+                environment = "{env}" \n\
+                transfer_executable = True \n\
+                queue 1 \n'.format(script = executable,
+                            mem    = mem,
+                            logs   = log_str,
+                            outl   = out_log,
+                            out    = out_folder,
+                            args   = args,
+                            env    = env,
+                            )
+
+            if submit_data:
+
+                # write submit file
+                sub_file = '%s/%s.sub' %(out_sub, log_str)
+                with open(sub_file, 'w') as f:
+                    f.write(submit_info)
+
+                # submit them
+                os.system("condor_submit {}".format(sub_file))
